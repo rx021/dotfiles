85d1bbc83cc808d8c239968db73e3ff1bcb78815
reinhardt021 <reinhardt.cgr@gmail.com>
2025-05-10 11:00:52 -0700

    dotfiles sync

---
+++ os-LINUX/config-bash.sh
@@ -0,0 +1,12 @@
+# REMOTE MACHINE DEV SETUP
+
+# // TODO: check for dependencies needed to run these configs
+
+# // TODO: for each of these files check if files already exists 
+# // if so then add to the end of the files
+# // if no then do the straight forward symlink
+ln -s ~/dotfiles/os-LINUX-remote/.bash_profile ~/.bash_profile
+# // note: use path relative to home (~/.../file) 
+# // not relative to pwd (./file) 
+
+
