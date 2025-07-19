7467e7243839f3ec7af3c212836eb4f5f06419f6
reinhardt <reinhardt.cgr@proton.me>
2025-07-19 01:33:10 -0700

    develop: WIP trying to get the TNS script alias working

--- os-SHARED/.alias_tmux.sh
+++ os-SHARED/.alias_tmux.sh
@@ -9,7 +9,20 @@ alias tkill="$(create_alias "tmux kill-session -t ")" #-- <name>
 alias trs='echo "// tmux rename-session -t <old-name> <new-name>"; tmux rename-session -t '
 
 ## SCRIPTS
+tns_script="~/dotfiles/SCRIPTS/tmux-new-session.sh"
+tns_msg="echo '// TRYING SCRIPT: $tns_script"
+run_tns()
+{
+  echo "run_tns --"
+  session_name=$1
+  sh "$tns_script" "$session_name"
+}
+alias tns="$tns_msg; run_tns "
 
-alias tns='echo "// sh ~/dotfiles/SCRIPTS/tmux-new-session.sh"; function foo(){ sh ~/dotfiles/SCRIPTS/tmux-new-session.sh $1; unset -f foo; }; foo'
+#-- original
+#alias tns='echo "// sh ${tns_script}"; function foo(){ sh ~/dotfiles/SCRIPTS/tmux-new-session.sh $1; unset -f foo; }; foo'
+
+#-- didn't work 
+#alias tns="$(create_script_alias "~/dotfiles/SCRIPTS/tmux-new-session.sh")"
 
 
