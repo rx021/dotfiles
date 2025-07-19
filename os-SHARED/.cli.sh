3ef2eab897d49a48200460d84842bad693aecdcb
reinhardt <reinhardt.cgr@proton.me>
2025-07-18 23:28:21 -0700

    develop: have the color & non color cli setup working

--- os-SHARED/.cli.sh
+++ os-SHARED/.cli.sh
@@ -12,39 +12,43 @@ alias ssha='eval $(ssh-agent); ssh-add'
 # $ lsb_release -a
 
 #-- modifying the bash prompt to something simpler
-#color_prompt=yes
-color_prompt=no
+#color_prompt=no
+color_prompt=yes
 
-cli_prefix="@"
+#cli_prefix="@"
+#cli_prefix="💧 "
 cli_prefix="💻 "
 
+host_name="\h"
 user_name="\u"
-working_dir="\W"
-cli_prompt="$"
 
-PS1="${cli_prefix}${working_dir}${cli_prompt}"
+present_working_dir="\w" #-- dir path
+curr_dir="\W" #-- curr dir only
+
+#cli_dir="${present_working_dir}"
+cli_dir="${curr_dir}"
+
+#cli_prompt="# "
+cli_prompt="$ "
+
+PS1="${cli_prefix}${cli_dir}${cli_prompt}"
 
 a_esc_char="\033" #-- ascii esc char
 a_blue="34m" #-- ascii blue
 a_purple="35m" #-- ascii purple
 a_white="37m" #-- ascii white
 prompt_color="${a_purple}"
-color_change="\[${a_esc_char}[${prompt_color}\]"
-color_reset="\[${a_esc_char}[${a_white}\]"
 
-PS1="${color_change}${cli_prefix}${working_dir}${color_reset}${cli_prompt} "
-#PS1="\[\033[34m\]${cli_prefix}\[\033[37m\]\$ "
-
-#PS1="\[\033[35m\]{cli_prefix} \W\[\033[37m\]\${cli_prompt} "
+server_data="${debian_chroot:+($debian_chroot)}"
 
 if [ "$color_prompt" = yes ]; then
-  #PS1="\[\033[35m\]@\W\[\033[37m\]\$ "
-  #PS1 = ${debian_chroot:+($debian_chroot)}\u@\h:\w\$
+  #PS1 = ${debian_chroot:+($debian_chroot)}${user_name}${cli_prefix}${host_name}:${present_working_dir}${cli_prompt}
   #ex: = root@ubuntu-s-1vcpu-1gb-sfo3-01:~#
-  #PS1="${debian_chroot:+($debian_chroot)}\[\033[34m\]\u@\h:\w\[\033[37m\]\$ "
-  #PS1="${debian_chroot:+($debian_chroot)}\[\033[34m\]\u💧\w\[\033[37m\]\$ "
-  #PS1="${debian_chroot:+($debian_chroot)}\[\033[34m\]\u💧\W\[\033[37m\]\$ "
-  PS1="${debian_chroot:+($debian_chroot)}\[\033[34m\]${cli_prefix}\W\[\033[37m\]\$ "
+
+  cli_color="\[${a_esc_char}[${prompt_color}\]"
+  color_reset="\[${a_esc_char}[${a_white}\]"
+
+  PS1="${cli_color}${cli_prefix}${cli_dir}${color_reset}${cli_prompt}"
 fi
 
 #--  alias to open NEOVIM quickly
