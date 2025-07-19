4b8272a3ea0e1585a3de4b510773f7b96645035e
reinhardt <reinhardt.cgr@proton.me>
2025-07-19 00:58:38 -0700

    develop: using helper method

--- os-SHARED/.alias_tmux.sh
+++ os-SHARED/.alias_tmux.sh
@@ -4,9 +4,12 @@ test_message="TEST TMUX aliases"
 alias alias_tmux="echo '$test_message'"
 
 alias tls="$(create_alias "tmux ls")"
-alias tns='echo "// sh ~/dotfiles/SCRIPTS/tmux-new-session.sh"; function foo(){ sh ~/dotfiles/SCRIPTS/tmux-new-session.sh $1; unset -f foo; }; foo'
-alias tas='echo "// tmux attach-session -t <name>"; tmux attach-session -t '
+alias tas="$(create_alias "tmux attach-session -t ")" #-- <name>
+alias tkill="$(create_alias "tmux kill-session -t ")" #-- <name>
 alias trs='echo "// tmux rename-session -t <old-name> <new-name>"; tmux rename-session -t '
-alias tkill='echo "// tmux kill-session -t <name>"; tmux kill-session -t '
+
+## SCRIPTS
+
+alias tns='echo "// sh ~/dotfiles/SCRIPTS/tmux-new-session.sh"; function foo(){ sh ~/dotfiles/SCRIPTS/tmux-new-session.sh $1; unset -f foo; }; foo'
 
 
