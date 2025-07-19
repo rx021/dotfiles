c43ccbdcc58525a1f85ff980fb97ed1975dec070
reinhardt <reinhardt.cgr@proton.me>
2025-07-18 20:15:24 -0700

    develop: improved route for bashrc mapping

--- os-LINUX-fedora/config-bash.sh
+++ os-LINUX-fedora/config-bash.sh
@@ -5,7 +5,8 @@
 # // TODO: for each of these files check if files already exists 
 # // if so then add to the end of the files
 # // if no then do the straight forward symlink
-ln -s ~/dotfiles/os-LINUX/.bash_profile ~/.bash_profile
+#ln -s ~/dotfiles/os-LINUX/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/os-LINUX-fedora/.bashrc ~/.bashrc
 # // note: use path relative to home (~/.../file) 
 # // not relative to pwd (./file) 
 
