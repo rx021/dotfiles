eb3be5c70ae34959266f9b8a88a0a22ec61d1eda
reinhardt <reinhardt.cgr@proton.me>
2026-02-16 14:01:19 -0800

    main: added PATH for Claude

--- os-SHARED/.cli_fedora.sh
+++ os-SHARED/.cli_fedora.sh
@@ -1,4 +1,8 @@
 
 #-- For laravel
 export PATH="/home/reinhardt/.config/herd-lite/bin:$PATH"
+
+# path update for Claude
+export PATH="$HOME/.local/bin:$PATH"
+
 export PHP_INI_SCAN_DIR="/home/reinhardt/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
