efb6c8ad26da30e7e5296c14147a2432d27a73fe
reinhardt <reinhardt.cgr@proton.me>
2026-02-19 12:29:07 -0800

    main: added PATH to use golang

--- os-SHARED/.cli_fedora.sh
+++ os-SHARED/.cli_fedora.sh
@@ -2,7 +2,10 @@
 #-- For laravel
 export PATH="/home/reinhardt/.config/herd-lite/bin:$PATH"
 
-# path update for Claude
+#-- path update for Claude
 export PATH="$HOME/.local/bin:$PATH"
+
+#-- path for Golang
+export PATH=$PATH:/usr/local/go/bin
 
 export PHP_INI_SCAN_DIR="/home/reinhardt/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
