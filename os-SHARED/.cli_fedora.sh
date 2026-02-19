f90ca738111f481efad5d4093d3d6c01fb10078e
reinhardt <reinhardt.cgr@proton.me>
2026-02-19 12:57:33 -0800

    main: finally fixed the GOPATH so that I can run lazysql

--- os-SHARED/.cli_fedora.sh
+++ os-SHARED/.cli_fedora.sh
@@ -6,6 +6,8 @@ export PATH="/home/reinhardt/.config/herd-lite/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
 #-- path for Golang
-export PATH=$PATH:/usr/local/go/bin
+export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"
+#export GOPATH="/home/reinhardt/go/bin/"
 
 export PHP_INI_SCAN_DIR="/home/reinhardt/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
