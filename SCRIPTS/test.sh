c8a449ede648b2c4ae1a872c0eeb774059445359
reinhardt021 <reinhardt.cgr@gmail.com>
2026-03-27 22:13:01 -0700

    main: testing shell script now working I just need to apply it to the alias

--- SCRIPTS/test.sh
+++ SCRIPTS/test.sh
@@ -1,7 +1,41 @@
-mydir='/usr/local/go/bin'
-if [ -d $mydir ]; then
-    echo "it exists";
+#mydir='/usr/local/go/bin'
+#if [ -d $mydir ]; then
+    #echo "it exists";
+#fi
+#if [ ! -d $mydir ]; then
+    #echo "it does not exists";
+#fi
+
+curr_shell=$SHELL
+if [[ "$SHELL" == *"bash" ]]; then
+  echo "HAVE BASH"
+elif [[ "$SHELL" == *"zsh" ]]; then
+  echo "HAVE ZSH"
+else
+  echo "no SHELL"
 fi
-if [ ! -d $mydir ]; then
-    echo "it does not exists";
+#this works fine
+#bash ./test.sh
+#this is the best way to do it
+
+whitelist="OBSIDIAN
+dotfiles
+OS25
+.os26
+.osYR
+"
+can_continue=false
+for curr_dir in "${whitelist[@]}"
+do
+  echo "$curr_dir/"
+  if echo "$PWD" | grep -q "$curr_dir";
+  then
+    echo "> on whitelist"
+    can_continue=true
+  fi
+done
+if [ "$can_continue" = false ]
+then
+  echo "> NOT on whitelist"
+  exit 1
 fi
