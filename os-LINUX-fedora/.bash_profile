8e634ff8f84056c50220f215d983d179991619c1
reinhardt <reinhardt.cgr@proton.me>
2026-05-06 16:08:30 -0700

    main: moved the rbenv init to work for fedora regardless of bash_profile or bashrc used

--- os-LINUX-fedora/.bash_profile
+++ os-LINUX-fedora/.bash_profile
@@ -22,7 +22,3 @@ source ~/dotfiles/os-SHARED/.pomodoro_fedora.sh
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-# Added by `rbenv init` on Wed May  6 03:46:19 PM PDT 2026
-eval "$(rbenv init - --no-rehash bash)"
-# commenting out for now since we'll use RVM
-# re-activating since RVM is having issues DL R-3
--- os-SHARED/.cli_fedora.sh
+++ os-SHARED/.cli_fedora.sh
@@ -11,3 +11,9 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 #export GOPATH="/home/reinhardt/go/bin/"
 
 export PHP_INI_SCAN_DIR="/home/reinhardt/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
+
+#-- For Ruby
+# Added by `rbenv init` on Wed May  6 03:46:19 PM PDT 2026
+eval "$(rbenv init - --no-rehash bash)"
+# commenting out for now since we'll use RVM
+# re-activating since RVM is having issues DL R-3
